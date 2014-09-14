1f52aa30e205605ae3d26e373adb4cfff8612a85
Emily Blatter <eblatter@hmc.edu>
2014-09-14 14:36:07 -0700

    finished ListManipulationExercise02

--- labs/src/main/scala/org/scalalabs/basic/lab02/ListManipulationExercise02.scala
+++ labs/src/main/scala/org/scalalabs/basic/lab02/ListManipulationExercise02.scala
@@ -11,7 +11,7 @@ object ListManipulationExercise02 {
    * As usual, various ways exist: pattern matching, folding, ...
    */
   def maxElementInList(l: List[Int]): Int = {
-    error("fix me")
+    l.max
   }
 
   /**
@@ -19,7 +19,7 @@ object ListManipulationExercise02 {
    * of the two list
    */
   def sumOfTwo(l1: List[Int], l2: List[Int]): List[Int] = {
-    error("fix me")
+    l1.zipAll(l2, 0, 0).map((x) => x._1 + x._2)
   }
 
   /**
@@ -27,7 +27,7 @@ object ListManipulationExercise02 {
    * method above
    */
   def sumOfMany(l: List[Int]*): List[Int] = {
-    error("fix me")
+    l.foldLeft(List.empty[Int])((l1, l2) => sumOfTwo(l1, l2))
   }
 
   case class Person(age: Int, firstName: String, lastName: String)
@@ -39,29 +39,9 @@ object ListManipulationExercise02 {
    * in a one-liner.
    */
   def separateTheYoungFromTheOld(persons: List[Person]): List[List[String]] = {
-    var youngins: ListBuffer[Person] = new ListBuffer[Person]()
-    var elders: ListBuffer[Person] = new ListBuffer[Person]()
-    var validYoungNames: ListBuffer[String] = new ListBuffer[String]()
-    var validOldNames: ListBuffer[String] = new ListBuffer[String]()
-
-    for (person <- persons) {
-        if (person.age < 18) {
-          youngins += person
-        } else {
-          elders += person
-        }
-    }
-
-    var sortedYoung = youngins.toList.sortBy(_.age)
-    var sortedOld = elders.toList.sortBy(_.age)
-
-    for (young <- sortedYoung) {
-      validYoungNames += young.firstName
-    }
-    for (old <- sortedOld) {
-      validOldNames += old.firstName
-    }
-    List(validYoungNames.toList, validOldNames.toList)
+    val sortedYoungAndOld = persons.sortWith((x,y) => x.age < y.age).partition((x) => x.age < 18)
+    
+    List(sortedYoungAndOld._1.map((x) => x.firstName), sortedYoungAndOld._2.map((x) => x.firstName))
   }
 
 }
